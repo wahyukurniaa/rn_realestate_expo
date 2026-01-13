0fb940b86a288999a065d63bf83c0d77f974586c
wahyukurniaa <wahyukurnia321123@gmail.com>
2026-01-13 21:50:16 +0700

    add-globalcontext-t0-root-_layout.tsx

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,14 +1,23 @@
-import {ActivityIndicator} from "react-native";
-import {SafeAreaView} from "react-native-safe-area-context";
-// import {Redirect, Slot} from "expo-router";
+import { Redirect, Slot } from "expo-router";
+import { ActivityIndicator } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function AppLayout(){
+// import { useGlobalContext } from "@/lib/global-provider";
 
-    return (
-        <SafeAreaView className="bg-white h-full flex justify-center items-center">
-            <ActivityIndicator className="text-primary-300" size="large" />
-        </SafeAreaView>
-    );
+export default function AppLayout() {
+    // const { loading, isLogged } = useGlobalContext();
 
+    if (loading) {
+        return (
+            <SafeAreaView className="bg-white h-full flex justify-center items-center">
+                <ActivityIndicator className="text-primary-300" size="large" />
+            </SafeAreaView>
+        );
+    }
+
+    // if (!isLogged) {
+    //     return <Redirect href="/sign-in" />;
+    // }
+
+    return <Slot />;
 }
-
